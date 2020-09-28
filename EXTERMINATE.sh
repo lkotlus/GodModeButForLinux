d1383da55415d44a884977be8d17a6454e54aaee
Logan <32753924+hufflegamer123@users.noreply.github.com>
2020-09-28 17:20:13 -0400

    Update EXTERMINATE.sh

--- EXTERMINATE.sh
+++ EXTERMINATE.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+#getting rid of empty lines
+sed -i '/^$/d' users.txt
+sed -i '/^$/d' users2.txt
+
+#Looping through 50 possible uids for users. This will work most of the time. Take care when looking at the badUsers file.
+for i in {1000..1050}
+do 
+    sudo grep -n $i /etc/passwd >> users.txt
+done
+
+sed -i '/^$/d' users.txt
+sed -i '/^$/d' users2.txt
+
+awk -F: '{ print $2}' users.txt > users2.txt
+cat users2.txt > users.txt
+
+sed -i '/^$/d' users.txt
+sed -i '/^$/d' users2.txt
+
+#Comparing users.txt to goodUsers.txt
+comm -2 -3 users.txt goodUsers.txt > badUsers.txt#!/bin/bash
+
 #Checking if the user is coming back after editing the badUsers.txt file
 echo '*_____________________________________*'
 echo '     have you come back after          ' 
@@ -132,6 +154,7 @@ then
 else
         echo 
         echo 'Then edit the badUsers.txt file.'
+        exit 1
 fi
 
 for i in ${badUsers[@]}
