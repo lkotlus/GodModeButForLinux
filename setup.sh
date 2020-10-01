ed4bdbc479c5351b452c1d6f27a422cfdee1e87f
Logan <32753924+hufflegamer123@users.noreply.github.com>
2020-10-01 16:27:34 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,14 @@
-    sudo wget https://raw.githubusercontent.com/hufflegamer123/GodModeButForLinux/master/EXTERMINATE.sh
-    sudo wget https://raw.githubusercontent.com/hufflegamer123/GodModeButForLinux/master/deleteFiles.sh
-    sudo wget https://raw.githubusercontent.com/hufflegamer123/GodModeButForLinux/master/passReq.sh
-    sudo touch users.txt
-    sudo touch users2.txt
-    sudo touch goodUsers.txt
-    sudo touch goodUsers2.txt
-    sudo touch badUsers.txt
-    sudo chmod +x EXTERMINATE.sh
-    echo 'Make sure you add a whitelist to goodUsers.txt'
-    sudo touch foundFiles.txt
-    sudo chmod +x deleteFiles.sh
-    sudo chmod +x passReq.sh
+#!bin/bash
+sudo wget https://raw.githubusercontent.com/hufflegamer123/GodModeButForLinux/master/EXTERMINATE.sh
+sudo wget https://raw.githubusercontent.com/hufflegamer123/GodModeButForLinux/master/deleteFiles.sh
+sudo wget https://raw.githubusercontent.com/hufflegamer123/GodModeButForLinux/master/passReq.sh
+sudo touch users.txt
+sudo touch users2.txt
+sudo touch goodUsers.txt
+sudo touch goodUsers2.txt
+sudo touch badUsers.txt
+sudo chmod +x EXTERMINATE.sh
+echo 'Make sure you add a whitelist to goodUsers.txt'
+sudo touch foundFiles.txt
+sudo chmod +x deleteFiles.sh
+sudo chmod +x passReq.sh
