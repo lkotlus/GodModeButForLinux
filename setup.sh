667d5f0fd00022fc34df1eef4373719143d1cdb8
Logan <32753924+hufflegamer123@users.noreply.github.com>
2020-10-01 15:30:36 -0400

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,18 @@
+echo 'Would you like to set up EXTERMINATE, deleteFiles, and passReq? (y/n)'
+read -p 'y/n: ' RESP
+if [ $RESP == 'y' ]
+then
+    sudo touch users.txt
+    sudo touch users2.txt
+    sudo touch goodUsers.txt
+    sudo touch goodUsers2.txt
+    sudo touch badUsers.txt
+    sudo chmod +x EXTERMINATE.sh
+    echo 'Make sure you add a whitelist to goodUsers.txt'
+    sudo touch foundFiles.txt
+    sudo chmod +x deleteFiles.sh
+    sudo chmod +x passReq.sh
+else
+    echo 'Okay!'
+    exit 1
+fi
