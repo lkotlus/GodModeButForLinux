fec95d39ddac82116417d26391332b4f37dce49a
Logan <32753924+hufflegamer123@users.noreply.github.com>
2020-10-11 09:11:36 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
+
 sudo wget https://raw.githubusercontent.com/hufflegamer123/GodModeButForLinux/master/EXTERMINATE.sh
 sudo wget https://raw.githubusercontent.com/hufflegamer123/GodModeButForLinux/master/deleteFiles.sh
 sudo wget https://raw.githubusercontent.com/hufflegamer123/GodModeButForLinux/master/passReq.sh
 sudo wget https://raw.githubusercontent.com/hufflegamer123/GodModeButForLinux/master/admin.sh
+
 sudo chmod +x EXTERMINATE.sh
 sudo chmod +x deleteFiles.sh
 sudo chmod +x passReq.sh
 sudo chmod +x admin.sh
+
 sudo touch adminWhitelist.txt
 sudo touch sudoers.txt
 sudo touch admins.txt
@@ -17,3 +20,21 @@ sudo touch goodUsers.txt
 sudo touch goodUsers2.txt
 sudo touch badUsers.txt
 sudo touch foundFiles.txt
+
+echo "
+        _____           _  ___  ___          _               
+       |  __ \         | | |  \/  |         | |              
+       | |  \/ ___   __| | | .  . | ___   __| | ___          
+       | | __ / _ \ / _| | | |\/| |/ _ \ / _| |/ _ \         
+       | |_\ | (_) | (_| | | |  | | (_) | (_| |  __/         
+        \____/\___/ \__,_| \_|  |_/\___/ \__,_|\___|         
+                                                             
+                                                             
+  __ __          __      __            __                    __  
+ / / | |         | |    / _|           | (_)                 \ \ 
+| |  | |__  _   _| |_  | |_ ___  _ __  | |_ _ __  _   ___  _  | |
+| |  | |_ \| | | | __| |  _/ _ \| '__| | | | '_ \| | | \ \/ / | |
+| |  | |_) | |_| | |_  | || (_) | |    | | | | | | |_| |>  <  | |
+| |  |_.__/ \__,_|\__| |_| \___/|_|    |_|_|_| |_|\__,_/_/\_\ | |
+ \_\                                                          /_/ 
+                                                             "
