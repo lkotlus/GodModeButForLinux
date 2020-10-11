90c8713066c333b28b21ff264ddeed92f5579aec
Logan <32753924+hufflegamer123@users.noreply.github.com>
2020-10-11 08:50:12 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,11 @@
 sudo wget https://raw.githubusercontent.com/hufflegamer123/GodModeButForLinux/master/EXTERMINATE.sh
 sudo wget https://raw.githubusercontent.com/hufflegamer123/GodModeButForLinux/master/deleteFiles.sh
 sudo wget https://raw.githubusercontent.com/hufflegamer123/GodModeButForLinux/master/passReq.sh
+sudo wget https://raw.githubusercontent.com/hufflegamer123/GodModeButForLinux/master/admin.sh
+sudo touch adminsWhitelist.txt
+sudo touch sudoers.txt
+sudo touch admins.txt
+sudo touch isUser.txt
 sudo touch users.txt
 sudo touch users2.txt
 sudo touch goodUsers.txt
@@ -12,3 +17,4 @@ echo 'Make sure you add a whitelist to goodUsers.txt'
 sudo touch foundFiles.txt
 sudo chmod +x deleteFiles.sh
 sudo chmod +x passReq.sh
+sudo chmod +x admin.sh
