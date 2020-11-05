b33971fb7dde1ea1000957bba99e47bee5771c55
Logan <32753924+hufflegamer123@users.noreply.github.com>
2020-11-05 17:42:22 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,23 +1,47 @@
 #!/bin/bash
 
+#
+## downloading the scripts
+#
+
 sudo wget https://raw.githubusercontent.com/hufflegamer123/GodModeButForLinux/master/EXTERMINATE.sh
 sudo wget https://raw.githubusercontent.com/hufflegamer123/GodModeButForLinux/master/deleteFiles.sh
 sudo wget https://raw.githubusercontent.com/hufflegamer123/GodModeButForLinux/master/passReq.sh
 sudo wget https://raw.githubusercontent.com/hufflegamer123/GodModeButForLinux/master/admin.sh
 sudo wget https://raw.githubusercontent.com/hufflegamer123/GodModeButForLinux/master/services.sh
+sudo wget https://raw.githubusercontent.com/hufflegamer123/GodModeButForLinux/master/packages.sh
+
+#
+## downloading the whitelists/blacklists
+#
+
 sudo wget https://raw.githubusercontent.com/hufflegamer123/GodModeButForLinux/master/servicesWhitelist.txt
+sudo wget https://raw.githubusercontent.com/hufflegamer123/GodModeButForLinux/master/packageBlacklist.txt
+
+#
+## downloading the readmes
+#
 
 sudo wget https://raw.githubusercontent.com/hufflegamer123/GodModeButForLinux/master/EXTERMINATEreadme.md
 sudo wget https://raw.githubusercontent.com/hufflegamer123/GodModeButForLinux/master/deleteFilesreadme.md
 sudo wget https://raw.githubusercontent.com/hufflegamer123/GodModeButForLinux/master/passReqreadme.md
 sudo wget https://raw.githubusercontent.com/hufflegamer123/GodModeButForLinux/master/adminreadme.md
 sudo wget https://raw.githubusercontent.com/hufflegamer123/GodModeButForLinux/master/servicesreadme.md
+sudo wget https://raw.githubusercontent.com/hufflegamer123/GodModeButForLinux/master/packagesreadme.md
+
+#
+## giving executable permissions to the scripts
+#
 
 sudo chmod +x EXTERMINATE.sh
 sudo chmod +x deleteFiles.sh
 sudo chmod +x passReq.sh
 sudo chmod +x admin.sh
 sudo chmod +x services.sh
+
+#
+## making the necessary files to pass around information, read information from, and write information to.
+#
 
 sudo touch adminWhitelist.txt
 sudo touch sudoers.txt
@@ -33,6 +57,10 @@ sudo touch services.txt
 sudo touch services2.txt
 sudo touch servicesWhitelist2.txt
 sudo touch badServices.txt
+
+#
+## making a fancy littl ascii art
+#
 
 echo "
         _____           _  ___  ___          _               
