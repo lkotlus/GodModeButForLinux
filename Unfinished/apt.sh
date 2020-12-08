3f3f2687b4988f9c8544791ac21d7f449e1f3556
Logan <32753924+hufflegamer123@users.noreply.github.com>
2020-12-07 20:38:33 -0500

    Delete apt.sh

--- Unfinished/apt.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Adding automatic updates
-echo -e 'APT::Periodic::Update-Package-Lists "1";\nAPT::Periodic::Unattended-Upgrade "1";' | sudo tee /etc/apt/apt.conf.d/20auto-upgrades > /dev/null
-
-# Only adding the recommended repos (this is a tad more complicated)
-grep -r --include '*.list' '^deb ' /etc/apt/sources.list /etc/apt/sources.list.d/ > sources.txt
-sed -i 's/^.*\(http:.*\).*$/\1/' sources.txt
-awk -F' ' '{ print $1}' sources.txt
